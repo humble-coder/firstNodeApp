e7df60407de7c4236db1cbddd1afc081d6365627
Mark A Brown <mark.philosophe@gmail.com>
2014-04-29 15:51:41 -0400

    first commit

---
+++ data/mappings.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var path = require('path');
+
+var Datastore = require('nedb');
+
+var db = {
+	mappings: new Datastore({ filename: path.join(__dirname, 'mappings.db'), autoload: true })
+};
+
+
+
+var mappings = {
+	get: function(alias, callback) {
+		db.mappings.findOne({ alias: alias }, function (err, mapping) {
+			if (err || !mapping) { return callback(new Error('URL not found.')); }
+			callback(null, mapping.url);
+		});
+	},
+
+	create: function(alias, url, callback) {
+		db.mappings.insert({ alias: alias, url: url }, callback);
+	},
+
+	list: function(callback) {
+		db.mappings.find({}).sort({ alias: 1}).exec(callback);
+	}
+};
+
+module.exports = mappings;
